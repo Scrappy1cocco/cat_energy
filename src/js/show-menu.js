680d4c69ce875f5a3b1350d61b8c1a89785ddf5b
Scrappy1cocco <jedediah.shine100@gmail.com>
2018-10-25 19:36:05 +0300

    таблет index почти достилил

--- src/js/show-menu.js
+++ src/js/show-menu.js
@@ -1,14 +1,14 @@
-var navMain = document.querySelector('.header-menu__nav');
-var navToggle = document.querySelector('.header-menu__toggle');
+var navMain = document.querySelector('.header__nav');
+var navToggle = document.querySelector('.header__toggle');
 
-navMain.classList.remove('header-menu__nav--nojs');
+navMain.classList.remove('header__nav--nojs');
 
 navToggle.addEventListener('click', function () {
-	if (navMain.classList.contains('header-menu__nav--closed')) {
-		navMain.classList.remove('header-menu__nav--closed');
-		navMain.classList.add('header-menu__nav--opened');
+	if (navMain.classList.contains('header__nav--closed')) {
+		navMain.classList.remove('header__nav--closed');
+		navMain.classList.add('header__nav--opened');
 	} else {
-		navMain.classList.add('header-menu__nav--closed');
-		navMain.classList.remove('header-menu__nav--opened');
+		navMain.classList.add('header__nav--closed');
+		navMain.classList.remove('header__nav--opened');
 	}
 });
